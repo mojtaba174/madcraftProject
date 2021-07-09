19074bfa452f34e0f943c0f0604715c6e42d01a2
GG <bitgoin.buy@gmail.com>
2021-07-10 03:00:33 +0430

    naaaaa

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Mojtaba\PycharmProjects\madcraftProject\venv
+set VIRTUAL_ENV=F:\prije\madcraftProject\venv
 
 if not defined PROMPT set PROMPT=$P$G
 

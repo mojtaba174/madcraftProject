cc98a9e11f473284e4961267a6c128d878359493
mojtaba174 <mojtabaansari.83.01@gmail.com>
2021-07-15 01:10:03 +0430

    background fix

--- madcraftProject/urls.py
+++ madcraftProject/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -11,4 +12,6 @@ urlpatterns = [
     path("users/", include("users.urls")),
     path('articles/', include('articles.urls')),
     path('about/', views.About, name="about" )
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/urls.py
+++ users/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 app_name = "users"
 urlpatterns = [
-    path("login/", views.login_view, name="login"),
     path("signup/", views.signup_view, name="signup"),
     path('logout/', views.logout_view, name="logout"),
     path('dashboard/', views.dashboard_view, name="dashboard"),
     path('dashboard/detail/', views.UserDetail, name="detail"),
     path('changeemail/', views.ChangeEmail_view, name="changeEmail"),
     path('changepassword/', views.ChangePassword_view, name="changePassword"),
+    path("login/", views.login_view, name="login"),
 ]
